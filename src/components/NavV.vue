3806d1497922985555647f1b6aca2387f688e47e
Windwalk <windwalk-bushido@protonmail.com>
2022-02-17 01:16:02 +0100

    Fixed carousels. Added nav. Finished design for desktops viewport.

---
+++ src/components/NavV.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  name: "NavV",
+};
+</script>
+
+<template>
+  <div
+    class="group flex flex-col justify-between w-16 h-screen rounded-tr-3xl rounded-br-3xl z-10 fixed shadow-lg transition-all duration-200 ease-out hover:w-96 bg-gray-800 text-white"
+  >
+    <div class="flex flex-col justify-center items-center m-2 group-hover:m-4 text-3xl font-bold">
+      <a href="/">
+        <img src="../assets/logo.jpg" class="w-full h-full rounded-full shadow-lg" />
+      </a>
+      <h3 class="mt-4 hidden group-hover:inline">Vanilla Studio</h3>
+    </div>
+
+    <FAIcon :icon="['fas', 'quote-right']" class="text-3xl group-hover:hidden text-green-400" />
+    <FAIcon :icon="['fas', 'globe']" class="text-3xl group-hover:hidden text-green-400" />
+    <FAIcon :icon="['fas', 'laptop-code']" class="text-3xl group-hover:hidden mb-3 text-green-400" />
+
+    <div class="flex flex-col justify-center items-center w-full p-4 text-xl hidden group-hover:flex">
+      <p class="text-center">“Show me a man with a tattoo and I'll show you a man with an interesting past”</p>
+      <p class="mt-4 text-2xl font-medium">Jack London</p>
+    </div>
+
+    <div class="flex flex-row flex-wrap justify-center items-center m-4 text-lg hidden group-hover:flex">
+      <div class="flex flex-col justify-center items-center w-full">
+        <p>Let's do some cool tattoo!</p>
+        <p>Schedule the visit via:</p>
+      </div>
+      <a href="#" class="m-4 text-3xl transition-all ease-in duration-150 text-green-500 hover:text-green-200">
+        <FAIcon :icon="['fab', 'facebook']" />
+      </a>
+      <a href="#" class="m-4 text-3xl transition-all ease-in duration-150 text-green-500 hover:text-green-200">
+        <FAIcon :icon="['fab', 'instagram']" />
+      </a>
+      <a href="#" class="m-4 text-3xl transition-all ease-in duration-150 text-green-500 hover:text-green-200">
+        <FAIcon :icon="['fas', 'envelope']" />
+      </a>
+    </div>
+
+    <div class="m-4 flex flex-wrap flex-col justify-center items-center hidden group-hover:flex">
+      <p>2022.</p>
+      <div class="flex flex-wrap flex-row justify-center items-center mt-2 text-sm">
+        <p>Website by</p>
+        <a
+          href="https://milijan-mosic.surge.sh"
+          class="ml-1 transition-all ease-in duration-150 text-green-500 hover:text-green-200"
+          >Windwalk</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script>
 import CarouselV from "../components/CarouselV.vue";
+import NavV from "../components/NavV.vue";
 import axios from "axios";
 
 export default {
@@ -7,6 +8,7 @@ export default {
 
   components: {
     CarouselV,
+    NavV,
   },
 
   data() {
@@ -14,7 +16,10 @@ export default {
       url: "http://localhost:1337",
       landscape_images: [],
       square_images: [],
-      portrait_images: [],
+      square_images_odd: [],
+      portrait_images_1: [],
+      portrait_images_2: [],
+      portrait_images_3: [],
     };
   },
 
@@ -25,6 +30,9 @@ export default {
         .get(url)
         .then((response) => {
           let fetched_images = response["data"];
+          let temp_landscape_images = [];
+          let temp_square_images = [];
+          let temp_portrait_images = [];
 
           for (let y = 0; y < 3; y++) {
             let images = fetched_images[y.toString()];
@@ -33,16 +41,46 @@ export default {
             while (dont_stop_while_loop) {
               if (images.Images[i] != null) {
                 if (y == 0) {
-                  this.landscape_images[i] = this.url + images.Images[i].url;
+                  temp_landscape_images[i] = this.url + images.Images[i].url;
                 }
                 if (y == 1) {
-                  this.square_images[i] = this.url + images.Images[i].url;
+                  temp_square_images[i] = this.url + images.Images[i].url;
                 } else {
-                  this.portrait_images[i] = this.url + images.Images[i].url;
+                  temp_portrait_images[i] = this.url + images.Images[i].url;
                 }
                 i++;
               } else {
                 dont_stop_while_loop = false;
+              }
+            }
+          }
+          for (let i = 0; i < temp_landscape_images.length; i++) {
+            this.landscape_images[i] = temp_landscape_images[i];
+          }
+          // All tattoos that have square dimensions should be divided in 2 groups.
+          let e = 0;
+          let o = 0;
+          for (let i = 0; i < temp_square_images.length; i++) {
+            if (i % 2 == 0) {
+              this.square_images[e] = temp_square_images[i];
+              e++;
+            } else {
+              this.square_images_odd[o] = temp_square_images[i];
+              o++;
+            }
+          }
+          // All tattoos in portrait orientation should be divided in 3 groups.
+          // First: 1st, 4th, 7th.,.. Second: 2nd, 5th, 8th... Third: 3rd, 6th, 9th...
+          for (let i = 0; i < 3; i++) {
+            let x = 0;
+            for (let y = i; y < temp_portrait_images.length; y += 3) {
+              if (i == 0) {
+                this.portrait_images_1[x] = temp_portrait_images[y];
+              }
+              if (i == 1) {
+                this.portrait_images_2[x] = temp_portrait_images[y];
+              } else {
+                this.portrait_images_3[x] = temp_portrait_images[y];
               }
             }
           }
@@ -60,19 +98,24 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-wrap flex-row justify-center items-center w-full">
-    <a href="#landscape" class="hidden w-0 h-0"></a>
-    <CarouselV mrk="x" class="w-full" :tattoos="landscape_images" />
-  </div>
-  <div class="flex flex-wrap flex-row justify-center items-center w-full">
-    <CarouselV mrk="x" class="w-1/2" :tattoos="square_images" />
-    <CarouselV mrk="x" class="w-1/2" :tattoos="square_images" />
-  </div>
-  <div class="flex flex-wrap flex-row justify-center items-center w-full">
-    <CarouselV mrk="x" class="w-1/3" :tattoos="portrait_images" />
-    <CarouselV mrk="x" class="w-1/3" :tattoos="portrait_images" />
-    <CarouselV mrk="x" class="w-1/3" :tattoos="portrait_images" />
+  <div class="bg-black">
+    <NavV />
+    <div class="ml-16 p-4">
+      <div class="flex flex-wrap flex-row justify-center items-center w-full rounded-3xl">
+        <a href="#landscapes" class="hidden w-0 h-0"></a>
+        <CarouselV class="w-full" :tattoos="landscape_images" :slide_speed="3300" />
+      </div>
+      <div class="flex flex-wrap flex-row justify-center items-center w-full rounded-3xl">
+        <a href="#squares" class="hidden w-0 h-0"></a>
+        <CarouselV class="w-1/2" :tattoos="square_images" :slide_speed="4500" />
+        <CarouselV class="w-1/2" :tattoos="square_images_odd" :slide_speed="6600" />
+      </div>
+      <div class="flex flex-wrap flex-row justify-center items-center w-full rounded-3xl">
+        <a href="#portraits" class="hidden w-0 h-0"></a>
+        <CarouselV class="w-1/3" :tattoos="portrait_images_1" :slide_speed="7100" />
+        <CarouselV class="w-1/3" :tattoos="portrait_images_2" :slide_speed="5400" />
+        <CarouselV class="w-1/3" :tattoos="portrait_images_3" :slide_speed="2200" />
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped></style>
